0b55bba3016060433c9e67c5160c3b549a9748a8
Luka Bulatovic <luka.bulatovic.w@gmail.com>
2018-03-21 11:00:33 +0100

    Get started payload setup

--- app.js
+++ app.js
@@ -90,8 +90,9 @@ function receivedPostback(event){
     
       
     messages.sendTextMessageWithImage(senderID, "http://chat-bot.rs/assets/images/1920/ko_je_kodi_1920x1080.png");
-    messages.sendTextMessage(senderID, "Hello Mr Naghi! \n I'm Cody the 5th element of this team! Nice to meet you. \n How are you?");
-    
+    setTimeout(function(){
+      messages.sendTextMessage(senderID, "Hello Mr Naghi! \n I'm Cody the 5th element of this team! Nice to meet you. \n How are you?");
+    }, 1000);
       
     
 
